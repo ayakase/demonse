75828c4bb3f5b76b589085e290492be580aa3030
ayakase <wibu1892001@gmail.com>
2023-05-02 19:30:35 +0700

    test

--- Brute Force/server.js
+++ Brute Force/server.js
@@ -1,24 +1,25 @@
 const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
 let password = '1min';
 
-function generateStrings(length, callback) {
-  const generate = (prefix) => {
-    if (prefix.length === length) {
-      callback(prefix);
-      if (prefix === password) {
-        return true;
+function generateStrings(maxLength, callback) {
+  for (let length = 1; length <= maxLength; length++) {
+    const generate = (prefix) => {
+      if (prefix.length === length) {
+        callback(prefix);
+        if (prefix === password) {
+          return true;
+        }
+        return false;
+      }
+      for (let i = 0; i < chars.length; i++) {
+        if (generate(prefix + chars[i])) {
+          return true;
+        }
       }
       return false;
-    }
-    for (let i = 0; i < chars.length; i++) {
-      if (generate(prefix + chars[i])) {
-        return true;
-      }
-    }
-    return false;
-  };
-  generate('');
+    };
+    generate('');
+  }
 }
 
 generateStrings(4, (combination) => {
