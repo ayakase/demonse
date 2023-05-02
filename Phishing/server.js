74749a99e2bc7e5a5de52b8f3307ccc9419ae58f
ayakase <wibu1892001@gmail.com>
2023-05-02 13:33:50 +0700

    test

--- Phishing/server.js
+++ Phishing/server.js
@@ -52,6 +52,6 @@ app.post('/submit', (req, res) => {
         }
     });
 });
-app.listen(3001, () => {
-    console.log('Server started on port 3001');
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
 });
